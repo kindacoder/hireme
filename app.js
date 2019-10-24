6942f5d76fe5f9a7f8df68d94dfff5f3b253b629
kindacoder <code.ashutosh@gmail.com>
2019-10-24 12:55:16 +0530

    login signup done

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+//serving static files
+app.use(express.static(__dirname + "/public"));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-//serving static files
-app.use(express.static('public'));
+
+//routes
+var userRoutes = require("./routes/user");
+
+
 
 app.get('/', (req, res) => {
     res.render('index');
 })
+
+
+//use routes
+app.use("/users", userRoutes);
 
 //listen to the server
 app.listen(port, () => {
